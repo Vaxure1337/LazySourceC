a2c57d940333b1b27f9b005e4a0b2228ff941477
Vaxure <47508058+Vaxure1337@users.noreply.github.com>
2019-06-18 09:29:34 -0230

    Update install.h

--- LazySourceC/install.h
+++ LazySourceC/install.h
@@ -1,24 +1,4 @@
-#include <iostream>
-
-class install{
-    public:
-    static void sqlmap(){
-        system("sudo apt install sqlmap");
-    };
-    
-    static void nmap(){
-        system("sudo apt install nmap");
-    };
-    
-    static void bbqsql(){
-        system("sudo apt install bbqsql");
-    };
-    
-    static void jsql(){
-        system("sudo apt install jsql");
-    };
-    
-    static void sqlninja(){
-        system("sudo apt install sqlninja");
-    };
+class Install{
+public:
+    static void Nmap();
 };
